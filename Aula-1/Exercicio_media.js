d496cb3ccd0b1c901cecee25edd2e7c5c636d02e
Gustavo <gustavoluizmiranda405@gmail.com>
2023-07-12 19:18:41 -0300

    finalização exercicios de media

--- Aula-1/Exercicio_media.js
+++ Aula-1/Exercicio_media.js
@@ -1,6 +1,17 @@
 let prompt = require('prompt-sync')()
 
-nota = prompt("Qual a primeira nota: ")
-nota = prompt("Qual a primeira nota: ")
-nota = prompt("Qual a primeira nota: ")
-nota = prompt("Qual a primeira nota: ")
+nota1 = parseInt(prompt("Qual a primeira nota: "))
+nota2 = parseInt(prompt("Qual a segunda nota: "))
+nota3 = parseInt(prompt("Qual a terceira nota: "))
+nota4 = parseInt(prompt("Qual a quarta nota: "))
+
+let media = (nota1 + nota2 + nota3 + nota4)/4
+
+console.log("A média foi " + media)
+if(media >= 7){
+    console.log("O aluno foi aprovado")
+} else if (media < 5){
+    console.log("O aluno foi reprovado")
+} else {
+    console.log("O aluno está de recuperação")
+}
