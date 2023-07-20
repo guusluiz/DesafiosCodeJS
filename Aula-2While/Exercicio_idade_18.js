022eaf78576407de43786855fa5339c5e6c507e2
Gustavo <gustavoluizmiranda405@gmail.com>
2023-07-20 19:20:00 -0300

    pacote do node

--- Aula-2While/Exercicio_idade_18.js
+++ Aula-2While/Exercicio_idade_18.js
@@ -1,7 +1,21 @@
-const prompt = require('prompt-sync')()
-let idade
-do {
-    idade = prompt("Qual a idade? ")
-} while (idade < 18);
+function verificarMaiorIdade() {
+    console.log("Exercício 5 - Faça um programa que receba nome e idade do usuário, enquanto a resposta do usuário for menor que 18 anos continue a solicitar a idade e mostre uma mensagem quando a idade do usuário for maior que 18");
 
-console.log("Idade maior que 18");
+    let pessoa = {
+        nome: "",
+        idade: 0
+    }
+
+   
+    console.log("Sua idade deve ser maior que 18.");
+
+    do {
+        pessoa.idade = parseInt(prompt("Digite sua idade: "));
+        pessoa.nome = prompt("Digite seu nome: ");
+
+    } while (pessoa.idade < 18);
+    console.log("Idade válida!");
+    console.log("Objeto:", pessoa);
+    console.log(`${pessoa.nome}, sua idade ${pessoa.idade}, é válida!`);
+
+}
